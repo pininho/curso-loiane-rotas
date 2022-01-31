6c125200b6d003f2774c4d23d1f92eba35dcd52c
Alessandro Muras de Oliveira Pino <alessandropino@caern.local>
2022-01-31 11:00:18 -0300

    criado rotas simples

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { CursosComponent } from './cursos/cursos.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'cursos', component: CursosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
