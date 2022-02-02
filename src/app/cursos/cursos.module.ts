ae3f0e1dcac30bb15ab5e1cda50c4c90b8374024
Alessandro Muras de Oliveira Pino <alessandropino@caern.local>
2022-02-02 13:08:17 -0300

    criado modulo de rotas de funcionalidade cursos.routing.module.ts

--- src/app/cursos/cursos.module.ts
+++ src/app/cursos/cursos.module.ts
@@ -1,17 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosComponent } from './cursos.component';
+import { CursosRoutingModule } from './cursos.routing.module';
 import { CursosService } from './cursos.service';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    /*RouterModule*/
+    CursosRoutingModule
   ],
   exports: [],
   declarations: [
