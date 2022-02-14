5357eba5001444ee77ede688e1db0a9d698a9ba2
Alessandro Muras de Oliveira Pino <alessandropino@caern.com.br>
2022-02-14 10:10:00 -0300

    tela de login e ocultado menu navbar se nao autenticado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { AuthService } from './login/auth.service';
 //import { CursosModule } from './cursos/cursos.module';
 //import { AlunosModule } from './alunos/alunos.module';
 
@@ -23,7 +24,7 @@ import { LoginComponent } from './login/login.component';
     //AlunosModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
